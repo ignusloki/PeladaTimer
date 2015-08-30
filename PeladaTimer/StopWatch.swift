f7a7a679538f9bb6a4eeea626165cf74d2bb6fad
Felipe <admin@Vitors-Mac-mini.local>
2015-08-30 19:52:02 -0300

    Refatoramento do código tirando a funcionalidade de timer do viewController

--- PeladaTimer/StopWatch.swift
+++ PeladaTimer/StopWatch.swift
@@ -8,4 +8,41 @@
 
 import Foundation
 
+func timeIntervalToString(ti: NSTimeInterval) -> String? {
+    let dcf = NSDateComponentsFormatter()
+    dcf.zeroFormattingBehavior = .Pad
+    dcf.allowedUnits = (.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond)
+    return dcf.stringFromTimeInterval(ti)
+}
 
+public struct StopWatch {
+    
+    private var startTime: NSDate?
+    private var accumulatedTime: NSTimeInterval = 0.0
+    
+    public var elapsedTimeInterval: NSTimeInterval {
+        get {
+            return self.accumulatedTime + NSDate().timeIntervalSinceDate(self.startTime ?? NSDate())
+        }
+    }
+    
+    public var elapsedTimeString: String {
+        get {
+            return timeIntervalToString(self.elapsedTimeInterval) ?? "00:00:00"
+        }
+    }
+    
+    public mutating func start() {
+        self.startTime = NSDate()
+    }
+    
+    public mutating func stop() {
+        self.accumulatedTime += NSDate().timeIntervalSinceDate(self.startTime ?? NSDate())
+        self.startTime = nil
+    }
+    
+    public mutating func reset() {
+        self.accumulatedTime = 0.0
+        self.start()
+    }
+}
--- PeladaTimer/ViewController.swift
+++ PeladaTimer/ViewController.swift
@@ -11,11 +11,11 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
-    var localNotification:UILocalNotification = UILocalNotification()
-    var toPass:String!
-    var startTime = NSTimeInterval()
-    var timer = NSTimer()
     let shareData = SharingManager.sharedInstance
+    
+    var localNotification:UILocalNotification = UILocalNotification()
+    var timer = NSTimer()
+    var stopWatch : StopWatch = StopWatch()
 
     @IBOutlet weak var displayTimeLabel: UILabel!    
     
@@ -33,14 +33,14 @@ class ViewController: UIViewController {
     @IBAction func start(sender: AnyObject) {
         
         if !timer.valid {
-        let aSelector : Selector = "updateTime"
+            let aSelector : Selector = "updateTime"
             timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: aSelector, userInfo: nil, repeats: true)
-            startTime = NSDate.timeIntervalSinceReferenceDate()
+            self.stopWatch.start()
         }
     }    
     
     @IBAction func stop(sender: AnyObject) {
-        timer.invalidate()
+        self.stopWatch.stop()
     }
     
     func updateTime() {
@@ -49,38 +49,13 @@ class ViewController: UIViewController {
         
         //Find the difference between current time and start time.
         
-        var elapsedTime: NSTimeInterval = currentTime - startTime
-        var savedElapsedTime = Int(elapsedTime)
+        var elapsedTime: Int = Int(stopWatch.elapsedTimeInterval)
         
-        //calculate the hour
-        let hours = Int(elapsedTime / 3600)
-        elapsedTime -= (NSTimeInterval(hours) * 3600)
-        
-        //calculate the minutes in elapsed time.
-        
-        let minutes = Int(elapsedTime / 60.0)
-        
-        elapsedTime -= (NSTimeInterval(minutes) * 60)
-        
-        //calculate the seconds in elapsed time.
-        
-        let seconds = Int(elapsedTime)
-        
-        elapsedTime -= NSTimeInterval(seconds)
-        
-        //add the leading zero for minutes, seconds and millseconds and store them as string constants
-        
-        let strHour = String(format: "%02d", hours)
-        let strMinutes = String(format: "%02d", minutes)
-        let strSeconds = String(format: "%02d", seconds)
-        
-        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
-        
-        displayTimeLabel.text = "\(strHour):\(strMinutes):\(strSeconds)"
+        displayTimeLabel.text = "\(stopWatch.elapsedTimeString)"
         
         if let timeMax = shareData.timeMax {
         
-            if savedElapsedTime == timeMax * 60 && timeMax != 0 {
+            if elapsedTime == timeMax * 60 && timeMax != 0 {
                 
                 localNotification.alertAction = "Alarme do Timer!"
                 localNotification.alertBody = "Acabou o tempo!!"
@@ -88,7 +63,9 @@ class ViewController: UIViewController {
                 localNotification.fireDate = NSDate(timeIntervalSinceNow: 0)
                 UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
                 
+                self.stopWatch.stop()
                 timer.invalidate()
+                
                 var alert = UIAlertController(title: "Alerta", message: "Acabou o tempo!", preferredStyle: UIAlertControllerStyle.Alert)
                 alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
                 self.presentViewController(alert, animated: true, completion: nil)
@@ -97,7 +74,7 @@ class ViewController: UIViewController {
         
         if let timeAlarm = shareData.timeAlarm {
             
-            if (savedElapsedTime % (timeAlarm * 60)) == 0 && timeAlarm != 0 && minutes != 0 {
+            if (elapsedTime % (timeAlarm * 60)) == 0 && timeAlarm != 0 && timer.valid {
                 println("Intervalo de Alarme acionado!!")
                 AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
                 
